47e28fae1a92c0d69f97e6c62832fc5462034025
carlosbunn <carlos.bunn@gmail.com>
2018-01-09 18:30:51 -0200

    Update newdroplet.sh

--- newdroplet.sh
+++ newdroplet.sh
@@ -6,7 +6,7 @@ curl -X POST https://api.digitalocean.com/v2/droplets \
 -H 'Content-Type: application/json' \
 -H "Authorization: Bearer $DO_API_KEY" \
 -d '{"name":"calculatorservice", "region":"nyc3", "size":"512mb",
-     "image":"18325354", "ssh_keys":[gocd-carlos],
+     "image":"18325354", "ssh_keys":["17289971"],
      "user_data":"
 #cloud-config
 runcmd:
