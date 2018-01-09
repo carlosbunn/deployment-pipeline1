e75ad30ab887f537bc914f2d94726e20add0209c
carlosbunn <carlos.bunn@gmail.com>
2018-01-09 17:31:46 -0200

    Add files via upload

---
+++ newdroplet.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DO_API_KEY=$1
+
+curl -X POST https://api.digitalocean.com/v2/droplets \
+-H 'Content-Type: application/json' \
+-H "Authorization: Bearer $DO_API_KEY" \
+-d '{"name":"calculatorservice", "region":"nyc3", "size":"512mb",
+     "image":"18325354", "ssh_keys":[gocd-carlos],
+     "user_data":"
+#cloud-config
+runcmd:
+  - sh -x  < <(curl -s https://raw.githubusercontent.com/carlosbunn/deployment-pipeline1/master/install.sh)
+"}'
