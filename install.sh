da25346474521d0455209e4dad8660d226f91db3
carlosbunn <carlos.bunn@gmail.com>
2018-01-09 19:00:17 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl https://download.gocd.org/gocd.repo -o /etc/yum.repos.d/gocd.repo
 yum -y install java-1.8.0-openjdk go-agent maven
-sed -i "s|https://127.0.0.1:8154/go|https://138.197.76.246:8154|g" /etc/default/go-agent
+sed -i "s|https://127.0.0.1:8154/go|https://138.197.76.246:8154/go|g" /etc/default/go-agent
 
 echo "agent.auto.register.key=49016d72-878d-48ca-8263-735d7eb26019
 agent.auto.register.resources=maven,java
