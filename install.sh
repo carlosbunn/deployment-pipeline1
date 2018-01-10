cbb9789aeb3163e6da73c91036531f46b9577701
carlosbunn <carlos.bunn@gmail.com>
2018-01-10 17:25:13 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-
 echo "deb https://download.gocd.org /" | sudo tee /etc/apt/sources.list.d/gocd.list
 curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo apt-key add -
 
-add-apt-repository ppa:webupd8team/java
+add-apt-repository ppa:webupd8team/java -y
 apt-get update
-apt-get install oracle-java8-installer maven git go-agent
+apt-get install oracle-java8-installer maven git go-agent -y
 
 sed -i "s|https://127.0.0.1:8154/go|https://138.197.76.246:8154/go|g" /etc/default/go-agent
 sed -i -E 's|AGENT_WORK_DIR=.*$|AGENT_WORK_DIR=/app|g' /etc/default/go-agent
