6b9e5196310535b7b5c5f4686e983126e098abef
carlosbunn <carlos.bunn@gmail.com>
2018-01-10 00:18:11 -0200

    removed key from script

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+AGENT_KEY=$1
+
 curl https://download.gocd.org/gocd.repo -o /etc/yum.repos.d/gocd.repo
 yum -y install java-1.8.0-openjdk go-agent maven git
 sed -i "s|https://127.0.0.1:8154/go|https://138.197.76.246:8154/go|g" /etc/default/go-agent
 
-echo "agent.auto.register.key=49016d72-878d-48ca-8263-735d7eb26019
+echo "agent.auto.register.key=$AGENT_KEY
 agent.auto.register.resources=maven,java
 agent.auto.register.environments=QA
 agent.auto.register.hostname=Agent01" >> /var/lib/go-agent/config/autoregister.properties
