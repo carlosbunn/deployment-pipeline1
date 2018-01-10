67be8486b819212685f2bfbe64abf223eedb3575
carlosbunn <carlos.bunn@gmail.com>
2018-01-09 23:05:35 -0200

    added tag option

--- newdroplet.sh
+++ newdroplet.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 DO_API_KEY=$1
+TAG=$2
 
 curl -X POST https://api.digitalocean.com/v2/droplets \
 -H 'Content-Type: application/json' \
 -H "Authorization: Bearer $DO_API_KEY" \
 -d '{"name":"calculatorservice", "region":"nyc3", "size":"512mb",
      "image":"18325354", "ssh_keys":["17289971"],
+     "tags":["$TAG"]
      "user_data":"
 #cloud-config
 runcmd:
