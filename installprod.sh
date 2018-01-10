cb52ed598b3b72c535dbfc2702bb56f7cbf5e327
carlosbunn <carlos.bunn@gmail.com>
2018-01-10 05:22:25 -0200

    Update installprod.sh

--- installprod.sh
+++ installprod.sh
@@ -3,15 +3,16 @@
 AGENT_KEY=$1
 
 curl https://download.gocd.org/gocd.repo -o /etc/yum.repos.d/gocd.repo
-yum -y install java-1.8.0-openjdk git maven
+yum -y install java-1.8.0-openjdk go-agent maven git
 sed -i "s|https://127.0.0.1:8154/go|https://138.197.76.246:8154/go|g" /etc/default/go-agent
-mkdir /app
-
+sed -i -E 's|AGENT_WORK_DIR=.*$|AGENT_WORK_DIR=/app|g' /etc/default/go-agent
+mkdir -p /app/jar /app/config/
+chmod -R 777 /app
 
 echo "agent.auto.register.key=$AGENT_KEY
-agent.auto.register.resources=java
+agent.auto.register.resources=maven,java
 agent.auto.register.environments=PROD
-agent.auto.register.hostname=Agent02" >> /var/lib/go-agent/config/autoregister.properties
+agent.auto.register.hostname=$(hostname)" >> /app/config/autoregister.properties
 /etc/init.d/go-agent start
 
 curl https://raw.githubusercontent.com/carlosbunn/deployment-pipeline1/master/calculator-service > /etc/init.d/calculator-service
