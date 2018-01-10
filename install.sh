814f3dc320f6d8d1206e773835d32d0fea4b1bc5
carlosbunn <carlos.bunn@gmail.com>
2018-01-10 16:56:20 -0200

    changed to ubuntu

--- install.sh
+++ install.sh
@@ -2,8 +2,19 @@
 
 AGENT_KEY=$1
 
-curl https://download.gocd.org/gocd.repo -o /etc/yum.repos.d/gocd.repo
-yum -y install java-1.8.0-openjdk go-agent maven git
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+update-locale
+locale
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+
+echo "deb https://download.gocd.org /" | sudo tee /etc/apt/sources.list.d/gocd.list
+curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo apt-key add -
+
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer maven git go-agent
+
 sed -i "s|https://127.0.0.1:8154/go|https://138.197.76.246:8154/go|g" /etc/default/go-agent
 sed -i -E 's|AGENT_WORK_DIR=.*$|AGENT_WORK_DIR=/app|g' /etc/default/go-agent
 mkdir -p /app/jar /app/config/
@@ -17,3 +28,4 @@ agent.auto.register.hostname=$(hostname)" >> /app/config/autoregister.properties
 
 curl https://raw.githubusercontent.com/carlosbunn/deployment-pipeline1/master/calculator-service > /etc/init.d/calculator-service
 chmod 755 /etc/init.d/calculator-service
+
