979a99661b06bb4d1a8e9d05e0a98bd0ca021dd9
carlosbunn <carlos.bunn@gmail.com>
2018-01-10 00:14:53 -0200

    Update newdroplet.sh

--- newdroplet.sh
+++ newdroplet.sh
@@ -2,6 +2,7 @@
 
 DO_API_KEY=$1
 TAG=$2
+AGENT_KEY=$3
 
 curl -X POST https://api.digitalocean.com/v2/droplets \
 -H 'Content-Type: application/json' \
@@ -13,5 +14,5 @@ curl -X POST https://api.digitalocean.com/v2/droplets \
 #cloud-config
 runcmd:
   - curl -O https://raw.githubusercontent.com/carlosbunn/deployment-pipeline1/master/install.sh
-  - /bin/sh -x install.sh
+  - /bin/sh -x install.sh $AGENT_KEY
 "}'
