5fc24db0bdf43974c934e363c027df2e15be5d67
carlosbunn <carlos.bunn@gmail.com>
2018-01-10 04:32:42 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ yum -y install java-1.8.0-openjdk go-agent maven git
 sed -i "s|https://127.0.0.1:8154/go|https://138.197.76.246:8154/go|g" /etc/default/go-agent
 sed -i -E 's|AGENT_WORK_DIR=.*$|AGENT_WORK_DIR=/app|g' /etc/default/go-agent
 mkdir -p /app/jar
-chmod -R 777 /app
+chmod -R 775 /app
 
 echo "agent.auto.register.key=$AGENT_KEY
 agent.auto.register.resources=maven,java
