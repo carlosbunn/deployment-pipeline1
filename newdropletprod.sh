5ba38dafbe0394981fa1edc34252cf629c30e526
carlosbunn <carlos.bunn@gmail.com>
2018-01-11 15:52:42 -0200

    Update newdropletprod.sh

--- newdropletprod.sh
+++ newdropletprod.sh
@@ -7,7 +7,7 @@ if ( curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Beare
 curl -X POST https://api.digitalocean.com/v2/droplets \
 -H 'Content-Type: application/json' \
 -H "Authorization: Bearer $DO_API_KEY" \
--d '{"name":"calculatorservice01", "region":"nyc3", "size":"1gb",
+-d '{"name":"calculatorservice02", "region":"nyc3", "size":"1gb",
      "image":"30822370", "ssh_keys":["17289971"],
      "tags":["'$TAG'"],
      "user_data":"
