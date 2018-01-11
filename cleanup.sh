23a23989943478b9ed6b5704e9480e969d4fe654
carlosbunn <carlos.bunn@gmail.com>
2018-01-11 17:31:04 -0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -5,4 +5,4 @@ DO_API_KEY=$1
 curl -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer $DO_API_KEY" "https://api.digitalocean.com/v2/droplets?tag_name=build"
 curl -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer $DO_API_KEY" "https://api.digitalocean.com/v2/droplets?tag_name=production"
 
-sleep 10
+sleep 20
