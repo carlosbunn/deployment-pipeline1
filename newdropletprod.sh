315e24324971469a9a16c937a0f38a3dea199a29
carlosbunn <carlos.bunn@gmail.com>
2018-01-11 15:31:00 -0200

    Update newdropletprod.sh

--- newdropletprod.sh
+++ newdropletprod.sh
@@ -3,11 +3,11 @@
 DO_API_KEY=$1
 TAG=$2
 AGENT_KEY=$3
-
+if ( curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DO_API_KEY" "https://api.digitalocean.com/v2/droplets?tag_name=build" |grep -q '"total":0' ); then
 curl -X POST https://api.digitalocean.com/v2/droplets \
 -H 'Content-Type: application/json' \
 -H "Authorization: Bearer $DO_API_KEY" \
--d '{"name":"calculatorservice02", "region":"nyc3", "size":"1gb",
+-d '{"name":"calculatorservice01", "region":"nyc3", "size":"1gb",
      "image":"30822370", "ssh_keys":["17289971"],
      "tags":["'$TAG'"],
      "user_data":"
@@ -16,3 +16,4 @@ runcmd:
   - curl -O https://raw.githubusercontent.com/carlosbunn/deployment-pipeline1/master/installprod.sh
   - /bin/sh -x installprod.sh '$AGENT_KEY'
 "}'
+fi 
